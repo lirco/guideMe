9ffcfd431ace76fd94a23278a77f1ed1e7fdf99f
BigRomanov <big.romanov@gmail.com>
2013-12-29 22:08:35 +0200

    Added the action infrastructure

---
+++ Extension/action.js
@@ -0,0 +1,24 @@
+var Action = function() {
+  
+}
+
+Action.prototype.do = function()
+{
+
+}
+
+Action.prototype.undo = function()
+{
+
+
+}
+
+Action.prototype.pre = function()
+{
+
+}
+
+Action.prototype.post = function()
+{
+
+}
